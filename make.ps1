cd6ac8860ce5e7b5948472d1c077c2e6324fbf88
Tim Rayburn <tim@timrayburn.net>
2013-05-02 23:46:25 -0500

    Make.ps1 added

---
+++ make.ps1
@@ -0,0 +1,27 @@
+﻿function Exists-Item {
+    param(
+        [Parameter(Mandatory=$true, Position=1)]
+        $item,
+        [Parameter(Mandatory=$false, Position=2)]
+        $Yes,
+        [Parameter(Mandatory=$false, Position=3)]
+        $No,
+        [Parameter(Mandatory=$false, Position=4)]
+        $Always
+    )
+    if ((Get-Item $item -ea si) -ne $null) {
+        if ($Yes -ne $null) {
+            $item | &{ PROCESS { &$Yes } }
+        }
+    } else {
+        if ($No -ne $null) {
+            $item | &{ PROCESS { &$No } }
+        }
+    }
+    if ($Always -ne $null) {
+        $item | &{ PROCESS { &$Always } }
+    }
+}
+
+Exists-Item .\Output { Remove-Item -Recurse -Force $_ } -Always { mkdir $_ }
+.\OnRamper\OnRamper\bin\debug\Onramper.exe --source=.\Template --destination=.\Output --config=.\Config --execute=.\Onramper\.nuget\nuget.exe
